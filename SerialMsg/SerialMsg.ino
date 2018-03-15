129850a47967185c50a0e62d39478764e27c0387
DrPride <2146343874@qq.com>
2018-03-15 21:53:26 +0800

    3.15
    arduino

---
+++ SerialMsg/SerialMsg.ino
@@ -0,0 +1,16 @@
+char ch = '1';
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.write(ch);
+  while(1){
+    if(Serial.available()){
+      ch = Serial.read();
+      Serial.print(ch);
+    }
+  }
+}
